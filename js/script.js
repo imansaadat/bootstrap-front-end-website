03abb7bdeeb40d98dca612a4f5187681fe41dd2b
ImanSaadat <e.saadat72@gmail.com>
2022-08-16 16:36:20 +0430

    bootstrap-front-end website added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+navLinks.forEach((l) => {
+    l.addEventListener('click',()=>{
+      menuToggle.classList.toggle('show')
+    })
+})
